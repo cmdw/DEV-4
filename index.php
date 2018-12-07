e75a5f1cb9ed770d55fce8b20aba96b77a1b5204
Dreamagination <s.k.candra@gmail.com>
2018-12-07 10:23:18 +0700

    first

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <!-- Meta -->
+  <meta charset="utf-8">
+  <title>Ngulik Technology</title>
+  <meta name="description" content="Technlogy update for everyone!">
+  <meta name="author" content="CMDW">
+  <meta name="keywords" content="Nguliktech, Ngulik, Teknologi, Technology">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Favicons -->
+  <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="img/favicon.png">
+
+  <!--- Styles CSS-->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/navbar.css">
+  <link rel="stylesheet" type="text/css" href="css/header.css">
+  <link rel="stylesheet" type="text/css" href="css/content1.css">
+  <link rel="stylesheet" type="text/css" href="css/content2.css">
+  <link rel="stylesheet" type="text/css" href="css/content3.css">
+  <link rel="stylesheet" type="text/css" href="css/waves.css">
+  <link rel="stylesheet" type="text/css" href="css/separator.css">
+  <link rel="stylesheet" type="text/css" href="css/typed.css">
+  <link rel="stylesheet" type="text/css" href="css/mousewheel.css">
+  <link rel="stylesheet" type="text/css" href="css/return-to-top.css">
+  <link rel="stylesheet" type="text/css" href="css/footer.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/fontawesome.css" integrity="sha384-u5J7JghGz0qUrmEsWzBQkfvc8nK3fUT7DCaQzNQ+q4oEXhGSx+P2OqjWsfIRB8QT" crossorigin="anonymous">
+
+ </head>
+
+ <body>
+
+
+  <!-- Script JS -->
+  <script type="text/javascript" src="js/script.js"></script>
+  <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+
+ </body>
+
+</html>
